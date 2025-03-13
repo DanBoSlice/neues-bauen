22268f35c7433182cb4167e393ddaf85e35eaf0f
Daniel Bossert <daniel@danbos.de>
2025-03-13 14:57:38 +0100

    add more style elements

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to the 1920s</h1>
-      <h2>Weimar Republic Architecture</h2>
-      Viele Elemente auf dieser Seite können angeklickt werden.
-    </div>
+    <>
+      <div className="intro">
+        <h1>Welcome to the 1920s</h1>
+        <h2>Weimar Republic Architecture</h2>
+
+        <p>Hier geht es um dies und das</p>
+      </div>
+
+      <div className="windows"></div>
+
+      <p>Viele Elemente auf dieser Seite können angeklickt werden.</p>
+
+      <button>Lorem</button>
+    </>
   );
 }
 
