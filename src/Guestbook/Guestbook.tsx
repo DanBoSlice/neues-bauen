5fd96003b68b2ef444e4a5e742934a21b66a22f8
Daniel Bossert <daniel@danbos.de>
2025-03-18 21:18:03 +0100

    add first guestbook draft

--- src/Guestbook/Guestbook.tsx
+++ src/Guestbook/Guestbook.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import "./Guestbook.css";
+import './Guestbook.css';
+import { ReactComponent as Svg } from './guestbook.svg';
 
 export default function Guestbook() {
   return (
     <>
-      Guestbook
+      <div className="preview style1">
+        <Svg/>
+      </div>
+
+      Facade: Klinker, mixed clinker facade, clinker base, first floor clinker, stone base
     </>
   );
 }
